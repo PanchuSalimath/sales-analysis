f7ddae10fa6ff392f51991489202f2bc50b9dbde
Panchakshari Salimath <panchaksharisalimath6@gmail.com>
2026-05-27 19:27:03 +0530

    Delete Dashboard directory

--- Dashboard/app.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.title("Welcome to Dashboard")
-st.write("This is a simple dashboard created using Streamlit.")
-st.write("You can create awesome web applications using Streamlit.")
