be1b95ab2f65285e016d2c7bb41f13550acc2005
Panchakshari Salimath <panchaksharisalimath6@gmail.com>
2026-05-27 19:54:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,17 @@ import streamlit as st
 st.title("Welcome to Dashboard")
 st.write("This is a simple dashboard created using Streamlit.")
 st.write("You can create awesome web applications using Streamlit.")
+
+st.markdown("""
+<style>
+
+.main {
+    background-color: #071021;
+    color: white;
+}
+
+.stApp {
+    background-color: #071021;
+    color: white;
+}
+""")
