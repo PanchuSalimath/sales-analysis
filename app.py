be6e6a044218f4391691d3b9ef702cd8e933bb91
Panchakshari Salimath <panchaksharisalimath6@gmail.com>
2026-05-27 19:28:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title("Welcome to Dashboard")
+st.write("This is a simple dashboard created using Streamlit.")
+st.write("You can create awesome web applications using Streamlit.")
